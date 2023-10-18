6eef9dd69649ed2286bd302379fdee8506e0be9f
Maruca Cantu Valdes <A00834245@itesm.mx>
2023-10-18 11:51:40 -0600

    Template
    Template de la app incluyendo primeras vistas

---
+++ manitasAPP.mc/DetalleOrdenView.swift
@@ -0,0 +1,20 @@
+//
+//  DetalleOrdenView.swift
+//  manitasAPP.mc
+//
+//  Created by Alumno on 18/10/23.
+//
+
+import SwiftUI
+
+struct DetalleOrdenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetalleOrdenView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetalleOrdenView()
+    }
+}
